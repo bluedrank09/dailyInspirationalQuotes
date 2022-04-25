3501d299854f8dcbfcf821eaff70c069b42ecc30
mayababu <mayababu73@gmail.com>
2022-04-25 16:27:43 +1200

    Added HTML code

--- dd_email.py
+++ dd_email.py
@@ -14,7 +14,7 @@ class dailyDigestEmail:
     def send_email(self):
         pass
 
-    def format_messgae(self):
+    def format_message(self):
         today = datetime.date.today()
         text = f"DAILY DIGEST - {today:%B, %d, %Y}"
 
@@ -31,8 +31,61 @@ class dailyDigestEmail:
             text+= "RANDOM WIKIPEDIA ARTICLE : "
             text+= f'{self.content["wikipedia"]["content"]["title"]} {self.content["wikipedia"]["content"]["extract"]}'
 
-            
-    print(f"SUCESS")
+        today = datetime.date.today()
+        html = f"""<html>
+        <body>
+        <center>
+            <h1>Daily Digest - {today:%B, %d, %Y}</h1>
+            """
+
+        if self.content['quote']['include'] and self.content['quote']['content']:
+            html += f"""
+        <h2>Quote of the Day</h2>
+        <i>"{self.content['quote']['content']['quote']}"</i> - {self.content['quote']['content']['author']}
+        """
+
+        if self.content['weather']['include'] and self.content['weather']['content']:
+            html += f"""
+        <h2>Forecast for {self.content['weather']['content']['city']}, {self.content['weather']['content']['country']}</h2> 
+        <table>
+                    """
+
+        for forecast in self.content['weather']['content']['periods']:
+            html += f"""
+        <tr>
+            <td>
+                {forecast['timestamp'].strftime('%d %b %H%M')}
+            </td>
+            <td>
+                <img src="{forecast['icon']}">
+            </td>
+            <td>
+                {forecast['temp']}\u00B0C | {forecast['description']}
+            </td>
+        </tr>
+                    """               
+
+        html += """
+        </table>
+                """
+
+        if self.content['wikipedia']['include'] and self.content['wikipedia']['content']:
+            html += f"""
+        <h2>Daily Random Learning</h2>
+        <h3><a href="{self.content['wikipedia']['content']['url']}">{self.content['wikipedia']['content']['title']}</a></h3>
+        <table width="800">
+            <tr>
+                <td>{self.content['wikipedia']['content']['extract']}</td>
+            </tr>
+        </table>
+                    """
+                    
+        html += """
+    </center>
+    </body>
+</html>
+                """
+
 
 if __name__ == "__main__":
     try:
@@ -44,3 +97,7 @@ if __name__ == "__main__":
 
     finally:
         print(f":D")
+
+
+
+        
